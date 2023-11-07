ed27d0d69609e78c60f92e1b8a288d0da323da98
Salisu Abubakar <salisbell404@gmail.com>
2023-11-07 11:18:27 +0100

    BRZ: renaming all to bronzecoin

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -10,11 +10,11 @@ BUILDDIR=${BUILDDIR:-$TOPDIR}
 BINDIR=${BINDIR:-$BUILDDIR/src}
 MANDIR=${MANDIR:-$TOPDIR/doc/man}
 
-BITCOIND=${BITCOIND:-$BINDIR/litecoind}
-BITCOINCLI=${BITCOINCLI:-$BINDIR/litecoin-cli}
-BITCOINTX=${BITCOINTX:-$BINDIR/litecoin-tx}
-WALLET_TOOL=${WALLET_TOOL:-$BINDIR/litecoin-wallet}
-BITCOINQT=${BITCOINQT:-$BINDIR/qt/litecoin-qt}
+BITCOIND=${BITCOIND:-$BINDIR/bronzecoind}
+BITCOINCLI=${BITCOINCLI:-$BINDIR/bronzecoin-cli}
+BITCOINTX=${BITCOINTX:-$BINDIR/bronzecoin-tx}
+WALLET_TOOL=${WALLET_TOOL:-$BINDIR/bronzecoin-wallet}
+BITCOINQT=${BITCOINQT:-$BINDIR/qt/bronzecoin-qt}
 
 [ ! -x $BITCOIND ] && echo "$BITCOIND not found or not executable." && exit 1
 
